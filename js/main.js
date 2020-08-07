43fe5858425e76a4af5819c79424e9db01768e5c
Felipe-CE <felipe_rsr@live.com>
2020-08-07 17:25:30 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,11 +10,16 @@ function cloneField() {
   const newFieldsContainer = document.querySelector('.schedule-item').cloneNode(true)
 
   // limpar os campos
-  const fielsd = newFieldsContainer.querySelectorAll('input')
-  fielsd.forEach(function (field) {
+  const fields = newFieldsContainer.querySelectorAll('input')
+  fields.forEach((field) => {
     field.value = "";
   });
   // duplicar campos
   document.querySelector('#schedule-items').appendChild(newFieldsContainer)
   // colcoar na pagina
 }
+
+// const fielsd = newFieldsContainer.querySelectorAll('input')
+// fielsd.forEach(function (field) {
+//   field.value = "";
+// });
