d8571ad163eab56fd568d5609ea7733def9bdb1c
Felipe-CE <felipe_rsr@live.com>
2020-08-09 12:15:25 -0300

    update

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const proffys = [
     time_from: [720],
     time_out: [1220]
   },
-  {  
+  {
     name: "Daniele",
     avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
     whatsapp: "8899998887",
@@ -23,19 +23,31 @@ const proffys = [
   }
 ]
 
+function pageLanding(req, res) {
+  return res.sendFile("index.html")
+}
+function pageStudy(req, res) {
+  return res.sendFile("study.html", { diego: proffys[0] })
+}
+function pagegiveClasses(req, res) {
+  return res.sendFile("give-classes.html")
+}
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
+
+// configurar nunjucks
+const nunjucks = requier('nunjucks')
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true,
+})
 
 
-  .get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
-  })
-  .get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html")
-  })
-  .get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html")
-  })
+server
+  // configurar arquivos estaticos (css, sripts, imagens)
+  .use(express.static("public"))
+  .get("/", pageLanding)
+  .get("/study", pageStudy)
+  .get("/give-classes", pagegiveClasses)
   .listen(5500)
