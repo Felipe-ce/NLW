0bb61f49b3792406326ffd591203d1d2b5e5f402
Felipe-CE <felipe_rsr@live.com>
2020-08-09 21:21:22 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,51 +1,52 @@
-// const proffys = [
-//   {
-//     name: "Diego Fernandes",
-//     avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-//     whatsapp: "8899998887",
-//     bio: 'Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratórioe por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.',
-//     subject: "Quimica",
-//     cost: "20",
-//     weekday: [0],
-//     time_from: [720],
-//     time_out: [1220]
-//   },
-//   {
-//     name: "Daniele",
-//     avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-//     whatsapp: "8899998887",
-//     bio: 'Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratórioe por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.',
-//     subject: "Quimica",
-//     cost: "20",
-//     weekday: [1],
-//     time_from: [720],
-//     time_out: [1220]
-//   }
-// ]
+const proffys = [
+  {
+    name: "Diego Fernandes",
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+    whatsapp: "8899998887",
+    bio: 'Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratórioe por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.',
+    subject: "Quimica",
+    cost: "20",
+    weekday: [0],
+    time_from: [720],
+    time_out: [1220]
+  },
+  {
+    name: "Daniele",
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+    whatsapp: "8899998887",
+    bio: 'Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratórioe por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.',
+    subject: "Quimica",
+    cost: "20",
+    weekday: [1],
+    time_from: [720],
+    time_out: [1220]
+  }
+]
 
 function pageLanding(req, res) {
-  return res.sendFile(__dirname + "/views/index.html")
+  return res.render("index.html")
 }
 function pageStudy(req, res) {
-  return res.sendFile(__dirname + "/views/study.html")
+  return res.render("study.html")
 }
 function pagegiveClasses(req, res) {
-  return res.sendFile(__dirname + "/views/give-classes.html")
+  return res.render("give-classes.html")
 }
-
-
-// // configurar nunjucks
-// const nunjucks = requier('nunjucks')
-// nunjucks.configure('src/views', {
-//   express: server,
-//   noCache: true,
-// })
 
 const express = require('express')
 const server = express()
+const nunjucks = require('nunjucks')
 
+// Configurar nunjucks
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true,
+})
+
+
+// configurando arquivos estaticos
 server.use(express.static('public'))
-
+  // rotas da aplicação
   .get("/", pageLanding)
   .get("/study", pageStudy)
   .get("/give-classes", pagegiveClasses)
@@ -56,5 +57,3 @@ server.use(express.static('public'))
         COMANDO : npm run dev
   
   */
-
-  /* testenteo o merge */
